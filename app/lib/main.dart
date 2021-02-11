9ddc6375455faf70de8be1b0928863fb6810afdf
Andreas Velounias <>
2021-02-11 18:07:50 +0000

    Style widgets

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -50,7 +51,39 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: transactions.map((tx) {
                 return Card(
-                  child: Text(tx.title),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 16.0),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: Colors.purple, width: 2.0)),
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "\$${tx.amount.toString()}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24.0,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat('MMMM dd, yyyy').format(tx.date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 );
               }).toList(),
             )
